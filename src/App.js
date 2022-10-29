67aa409dfc06d2ed25a7d92a11f75095a541ae48
Sara <sara@gmail.com>
2022-10-30 00:18:49 +0300

    the HW

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav  from './components/Nav';
+import Foot from './components/Foot';
+import Store from './components/Store';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <Nav/>
+    <div className='c1'>
+    <Store name="دورة تجربة المستخدم" date="17 Nov-30 Nov" aval="17" coruseimg="https://www.webdesignerdepot.com/cdn-origin/uploads/2018/05/UX.jpg"/>
+    <Store  name=" معسكر تطوير المواقع" date="15 Nov-20 Nov" aval=" 15" coruseimg="https://www.trio.dev/hubfs/Imported_Blog_Media/263a75529a1752b75d64f9f21fd07c92-3-2.jpg"/>
+    <Store  name="معسكر بايثون" date="1 Nov-30 Nov" aval=" 1" coruseimg="https://ih1.redbubble.net/image.523099154.3559/flat,750x,075,f-pad,750x1000,f8f8f8.u5.jpg"/>
+    <Store  name="دورة جافا" date="27 Nov-30 Nov" aval="27" coruseimg="https://cdn.arageek.com/magazine/2021/04/encyclopedia-%D9%85%D8%A7-%D9%87%D9%8A-%D8%B9%D9%8A%D9%88%D8%A8-%D9%84%D8%BA%D8%A9-%D8%AC%D8%A7%D9%81%D8%A7.jpg"/>
+    <Store  name="معسكر أمن سيبراني" date=" 30 Oct-30 Nov" aval="30" coruseimg="https://albilad.s3.me-south-1.amazonaws.com/images/news/2021/02/21/f21164558.jpg"/>
+    <Store  name="معسكر علم البيانات" date="1 Nov-13 Nov"aval="1" coruseimg="https://pbs.twimg.com/media/DPwBYdHWAAIWnNT.jpg" />
+    </div>
+    <Foot/>
+    
     </div>
   );
 }
---
+++ src/components/Store.js
@@ -0,0 +1,112 @@
+import {
+    Box,
+    Center,
+    useColorModeValue,
+    Heading,
+    Text,
+    Stack,
+    Image,
+  } from '@chakra-ui/react';
+  import pic from '../image/pic.webp';
+
+  
+//   const IMAGE =
+//     'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80';
+  
+  export default function Store(props) {
+    let avilable ;
+     let color;
+    if(props.aval>4){
+      avilable ="Available";
+      color="#43a047";
+      
+
+    }
+    else{
+      avilable="Not Available";
+color="#d50000";
+    }
+    return (
+ 
+      <Center py={12}>
+        <Box 
+          role={'group'}
+          p={6}
+          maxW={'330px'}
+          w={'full'}
+          bg={useColorModeValue('white', 'gray.800')}
+          boxShadow={'2xl'}
+          rounded={'lg'}
+          pos={'relative'}
+          zIndex={1}>
+          <Box
+            rounded={'lg'}
+            mt={-12}
+            pos={'relative'}
+            height={'230px'}
+            _after={{
+              transition: 'all .3s ease',
+              content: '""',
+              w: 'full',
+              h: 'full',
+              pos: 'absolute',
+              top: 5,
+              left: 0,
+            //   backgroundImage: `url(${imgt})`,
+              filter: 'blur(15px)',
+              zIndex: -1,
+            }}
+            _groupHover={{
+              _after: {
+                filter: 'blur(20px)',
+              },
+            }}>
+            <Image
+          
+             img src={props.coruseimg}  
+             
+              rounded={'lg'}
+              height={230}
+              width={282}
+              objectFit={'cover'}
+             
+             />
+
+          </Box>
+          <Stack pt={10} align={'center'}>
+           
+          
+            <Heading fontSize={'18px'} fontFamily={'body'} fontWeight={500}>
+             {props.name}
+            </Heading>
+
+            <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+            {props.date}
+            </Text>
+           
+            <Stack direction={'row'} align={'center'}>
+              {/* <Text fontWeight={800} fontSize={'xl'}>
+              {props.state}
+
+              </Text> */}
+              <text 
+              fontSize={'sm'}
+              fontWeight={'500'}
+              align={'center'}
+          
+              p={2}
+              px={3}
+              color={'black'}
+              rounded={'full'}
+
+
+              >
+              {avilable}
+              </text>
+           
+            </Stack>
+          </Stack>
+        </Box>
+      </Center>
+    );
+  }
